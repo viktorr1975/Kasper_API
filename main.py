be6b943c411818d2eeecdf89fcb6c15c4bf3d5db
vit <viktorr1975@yandex.com>
2025-05-15 17:11:32 +0700

    Рабочий вариант поиска устройства по имени

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from KlAkOAPI.AdmServer import KlAkAdmServer
 from KlAkOAPI import Updates
+from KlAkOAPI import HostGroup
+from KlAkOAPI import ChunkAccessor
 from datetime import timedelta
 
 import urllib3
@@ -29,6 +31,7 @@ def ConnectKSC(ip):
             return None
 
 def get_status_hosts(server, ip):
+#получение информации о датах обновлений
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         params = []
         if server is not None:
@@ -51,7 +54,33 @@ def get_status_hosts(server, ip):
         else:
             print('Ошибка доступа к серверу')
 
+def get_host_info(server, strQueryString):
+#получение информации об устройстве
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    params = []
+    if server is not None:
+        oHostGroup = HostGroup.KlAkHostGroup(server)
+        strAccessor = oHostGroup.FindHosts(
+            strQueryString, ["KLHST_WKS_HOSTNAME", "KLHST_WKS_DN"],
+            [], {'KLGRP_FIND_FROM_CUR_VS_ONLY': True},
+            lMaxLifeTime=60 * 60 * 3).OutPar('strAccessor')
+
+        nStart = 0
+        nStep = 100
+        oChunkAccessor = ChunkAccessor.KlAkChunkAccessor(server)
+        nCount = oChunkAccessor.GetItemsCount(strAccessor).RetVal()
+        print("Found hosts count:", nCount)
+
+        while nStart < nCount:
+            oChunk = oChunkAccessor.GetItemsChunk(strAccessor, nStart, nStep)
+            parHosts = oChunk.OutPar('pChunk')['KLCSP_ITERATOR_ARRAY']
+            for oObj in parHosts:
+                print('Found host: ' + oObj['KLHST_WKS_DN'])
+            nStart += nStep
+    else:
+        print('Ошибка доступа к серверу')
+
 if __name__ == '__main__':
     for ip in KSC_LIST.values():
         server = ConnectKSC(ip)
-        get_status_hosts(server, ip)
+        get_host_info(server, 'KLHST_WKS_DN = "WIN*"')
