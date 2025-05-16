afe27bb62982b58d1b8a25bde897870af9a810dd
vit <viktorr1975@yandex.com>
2025-05-16 21:56:49 +0700

    Базовый функционал по получению данных устройства реализовал

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ from KlAkOAPI import ChunkAccessor
 from datetime import timedelta
 
 import urllib3
-
+import socket
+import struct
+import csv
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 username = 'administrator'
 password = '1qazXSW@'
+#!!!!!!!!!!!!!!!!!!!!!!!!!
+
 
 KSC_LIST = {
     'WINDOWS': 'https://192.168.122.181:13299', # VM win2k16-3
@@ -54,14 +59,45 @@ def get_status_hosts(server, ip):
         else:
             print('Ошибка доступа к серверу')
 
+def convert_int_to_ip(n):
+# convert integer to IP4 address
+# IP4 addresses can be represented in big-endian byte order,
+    return socket.inet_ntoa(struct.pack('<I', n))
+
+def save_to_csv(lstHostsData, strFileName="hosts.csv"):
+# сохраняем список с данными хостов в файл формата CSV
+    # список заголовков для данных хоста
+    replacements = {
+        "KLHST_WKS_DN": 'Имя',
+        "KLHST_WKS_IP": 'IP',
+        "KLHST_WKS_GROUPID": 'Группа'}
+    # Extract all unique keys (headers)
+    fieldnames = set()
+    for entry in lstHostsData:
+        fieldnames.update(entry.keys())
+    fieldnames = list(fieldnames)
+    # For replace the field names (headers)
+#    fieldnames = ["KLHST_WKS_DN", "KLHST_WKS_IP", "KLHST_WKS_GROUPID"]
+    headers = {x:replacements.get(x) for x in fieldnames}
+
+#my_list = list(map(lambda x: new_value if x == old_value else x, my_list))
+    # Writing to CSV
+    with open(strFileName, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writerow(headers)    # write the header
+        #writer.writeheader(headers)  # Write header row
+        writer.writerows(lstHostsData)  # Write data rows
+
 def get_host_info(server, strQueryString):
 #получение информации об устройстве
+# strQueryString - Host display name
+# примеры strQueryString: "*Name*", "nAME",
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     params = []
     if server is not None:
         oHostGroup = HostGroup.KlAkHostGroup(server)
         strAccessor = oHostGroup.FindHosts(
-            strQueryString, ["KLHST_WKS_HOSTNAME", "KLHST_WKS_DN"],
+            'KLHST_WKS_DN = "' + strQueryString + '"', ["KLHST_WKS_GROUPID", "KLHST_WKS_DN", "KLHST_WKS_IP"],
             [], {'KLGRP_FIND_FROM_CUR_VS_ONLY': True},
             lMaxLifeTime=60 * 60 * 3).OutPar('strAccessor')
 
@@ -69,18 +105,27 @@ def get_host_info(server, strQueryString):
         nStep = 100
         oChunkAccessor = ChunkAccessor.KlAkChunkAccessor(server)
         nCount = oChunkAccessor.GetItemsCount(strAccessor).RetVal()
-        print("Found hosts count:", nCount)
-
+#        print("Found hosts count:", nCount)
+        result = []
         while nStart < nCount:
             oChunk = oChunkAccessor.GetItemsChunk(strAccessor, nStart, nStep)
             parHosts = oChunk.OutPar('pChunk')['KLCSP_ITERATOR_ARRAY']
             for oObj in parHosts:
-                print('Found host: ' + oObj['KLHST_WKS_DN'])
+                host = {}
+                host["KLHST_WKS_DN"] = oObj['KLHST_WKS_DN']
+                host["KLHST_WKS_IP"] = convert_int_to_ip(oObj['KLHST_WKS_IP'])
+                host["KLHST_WKS_GROUPID"] = oHostGroup.GetGroupInfo(oObj['KLHST_WKS_GROUPID']).retval.GetValue('name')
+                result.append(host)
+#                print('Found host: ' + oObj['KLHST_WKS_DN'])
+#                print('Host IPv4 address with network byte order: ',  convert_int_to_ip(oObj['KLHST_WKS_IP']))
+#                print('Group : ' + oHostGroup.GetGroupInfo(oObj['KLHST_WKS_GROUPID']).retval.GetValue('name'))
             nStart += nStep
+        return result
     else:
         print('Ошибка доступа к серверу')
 
 if __name__ == '__main__':
     for ip in KSC_LIST.values():
         server = ConnectKSC(ip)
-        get_host_info(server, 'KLHST_WKS_DN = "WIN*"')
+        hosts = get_host_info(server, "*wIN*")
+        save_to_csv(hosts, "hosts1.csv")
